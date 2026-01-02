3c1f7a3e28277fea020a3f20f55465e8f5eaf024
RossWebb <rnlwebb@gmail.com>
2026-01-02 17:10:28 +1100

    Update service worker cache name and management
    Updated cache name to 'ttk-v2' and added cache management during activation.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ttk-v1';
+const CACHE_NAME = 'ttk-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -11,6 +11,26 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  // Force the waiting service worker to become active
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // Delete old caches
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Take control of all pages immediately
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
