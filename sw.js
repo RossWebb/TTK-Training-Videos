93d4a1dec9d6b5c3b066d47c4d48f5bad0d062f6
RossWebb <rnlwebb@gmail.com>
2026-05-12 08:27:07 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 2.5; // Just increment this number when you update
+const CACHE_VERSION = 2.6; // Just increment this number when you update
 const CACHE_NAME = `ttk-v${CACHE_VERSION}`;
 
 const urlsToCache = [
