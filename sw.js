ce0d78f8c87530615780f4626f93f5d1cb769d75
RossWebb <rnlwebb@gmail.com>
2026-09-24 22:59:10 -0700

    Increment CACHE_VERSION to 3.56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 3.55; // Just increment this number when you update
+const CACHE_VERSION = 3.56; // Just increment this number when you update
 const CACHE_NAME = `ttk-v${CACHE_VERSION}`;
 
 const urlsToCache = [
